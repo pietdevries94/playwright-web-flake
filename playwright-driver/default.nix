443f450ea39a6e04ab5125d74a59efd2ed64b0df
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2026-07-14 18:07:42 +0000

    fix: use PyPI wheels to get driver

--- playwright-driver/default.nix
+++ playwright-driver/default.nix
@@ -20,13 +20,31 @@ let
 
   driver = stdenv.mkDerivation (finalAttrs:
     let
-      suffix = {
-        x86_64-linux = "linux";
-        aarch64-linux = "linux-arm64";
-        x86_64-darwin = "mac";
-        aarch64-darwin = "mac-arm64";
+      wheel = {
+        x86_64-linux = {
+          filename = "playwright-1.57.0-py3-none-manylinux1_x86_64.whl";
+          url = "https://files.pythonhosted.org/packages/56/61/3a803cb5ae0321715bfd5247ea871d25b32c8f372aeb70550a90c5f586df/playwright-1.57.0-py3-none-manylinux1_x86_64.whl";
+          hash = "sha256-KE7Vpwa3w4mgbKpDGy8LqaxBMBE8OneXZ92nWMJJe7E=";
+        };
+
+        aarch64-linux = {
+          filename = "playwright-1.57.0-py3-none-manylinux_2_17_aarch64.manylinux2014_aarch64.whl";
+          url = "https://files.pythonhosted.org/packages/83/d7/b72eb59dfbea0013a7f9731878df8c670f5f35318cedb010c8a30292c118/playwright-1.57.0-py3-none-manylinux_2_17_aarch64.manylinux2014_aarch64.whl";
+          hash = "sha256-OKG65sCgeDnN6t28B1azsrheR2wHlF9k7OCPH5VqhvE=";
+        };
+
+        x86_64-darwin = {
+          filename = "playwright-1.57.0-py3-none-macosx_10_13_x86_64.whl";
+          url = "https://files.pythonhosted.org/packages/ed/b6/e17543cea8290ae4dced10be21d5a43c360096aa2cce0aa7039e60c50df3/playwright-1.57.0-py3-none-macosx_10_13_x86_64.whl";
+          hash = "sha256-k1HBrD39mzgg/n/ENA2WwNNza7aAl7m3ppvUXSXpNww=";
+        };
+
+        aarch64-darwin = {
+          filename = "playwright-1.57.0-py3-none-macosx_11_0_arm64.whl";
+          url = "https://files.pythonhosted.org/packages/8b/04/ef95b67e1ff59c080b2effd1a9a96984d6953f667c91dfe9d77c838fc956/playwright-1.57.0-py3-none-macosx_11_0_arm64.whl";
+          hash = "sha256-pKnWUCe85I7rqEJAi8wUIVAt/X5B4o0gfpQmD6k8pn4=";
+        };
       }.${system} or throwSystem;
-      filename = "playwright-${finalAttrs.version}-${suffix}.zip";
     in
     {
       pname = "playwright-driver";
@@ -34,16 +52,16 @@ let
       version = "1.57.0";
 
       src = fetchurl {
-        url = "https://cdn.playwright.dev/builds/driver/${filename}";
-        sha256 = {
-          x86_64-linux = "17wlr6mz48wa6k161pfpsssj16y4kwdy92lcn2cr48h48q87iyb7";
-          aarch64-linux = "1m14szp1cap89adzmqjs41q4w23n6xnlg5wh7clb07i9xg88wgzd";
-          x86_64-darwin = "0dx7axgp4y4bpqmydrgficnzh58w9b8gyrm89h4byy6rqij41v9f";
-          aarch64-darwin = "0ys4p1hfka3izbnxrblh9ql8q4jbi0nm9wcm27yq6ni2nxr6ivkz";
-        }.${system} or throwSystem;
+        inherit (wheel) url hash;
       };
 
-      sourceRoot = ".";
+      unpackPhase = ''
+        runHook preUnpack
+        unzip "$src"
+        runHook postUnpack
+      '';
+
+      sourceRoot = "playwright/driver";
 
       nativeBuildInputs = [ unzip ];
 
@@ -76,15 +94,15 @@ EOF
         runHook postInstall
       '';
 
-      passthru = rec {
-        inherit filename;
+      passthru = {
+        inherit (wheel) filename;
         browsers = {
-          x86_64-linux = browsers-linux-x64 { };
-          aarch64-linux = browsers-linux-arm64 { };
+          x86_64-linux = browsers-linux { };
+          aarch64-linux = browsers-linux { };
           x86_64-darwin = browsers-mac;
           aarch64-darwin = browsers-mac;
         }.${system} or throwSystem;
-        browsers-chromium = browsers;
+        browsers-chromium = browsers-linux { };
       };
     });
 
@@ -111,49 +129,7 @@ EOF
     meta.platforms = lib.platforms.darwin;
   };
 
-  browsers-linux-x64 = { withChromium ? true }:
-    let
-      fontconfig = makeFontsConf {
-        fontDirectories = [ ];
-      };
-    in
-    runCommand
-      ("playwright-browsers"
-        + lib.optionalString withChromium "-chromium")
-      {
-        nativeBuildInputs = [
-          makeWrapper
-          jq
-        ];
-      }
-      (''
-        BROWSERS_JSON=${driver}/package/browsers.json
-      '' + lib.optionalString withChromium ''
-        CHROMIUM_REVISION=$(jq -r '.browsers[] | select(.name == "chromium").revision' $BROWSERS_JSON)
-        mkdir -p $out/chromium-$CHROMIUM_REVISION/chrome-linux64
-
-        # See here for the Chrome options:
-        # https://github.com/NixOS/nixpkgs/issues/136207#issuecomment-908637738
-        makeWrapper ${chromium}/bin/chromium $out/chromium-$CHROMIUM_REVISION/chrome-linux64/chrome \
-          --set SSL_CERT_FILE /etc/ssl/certs/ca-bundle.crt \
-          --set FONTCONFIG_FILE ${fontconfig}
-
-        # We also need to install the headless shell version of Chromium
-        CHROMIUM_HEADLESS_SHELL_REVISION=$(jq -r '.browsers[] | select(.name == "chromium-headless-shell").revision' $BROWSERS_JSON)
-        mkdir -p $out/chromium-headless-shell-$CHROMIUM_HEADLESS_SHELL_REVISION/chrome-linux64
-
-        # See here for the Chrome options:
-        # https://github.com/NixOS/nixpkgs/issues/136207#issuecomment-908637738
-        makeWrapper ${chromium}/bin/chromium $out/chromium_headless_shell-$CHROMIUM_REVISION/chrome-headless-shell-linux64/chrome-headless-shell \
-          --set SSL_CERT_FILE /etc/ssl/certs/ca-bundle.crt \
-          --set FONTCONFIG_FILE ${fontconfig}
-      '' + ''
-        FFMPEG_REVISION=$(jq -r '.browsers[] | select(.name == "ffmpeg").revision' $BROWSERS_JSON)
-        mkdir -p $out/ffmpeg-$FFMPEG_REVISION
-        ln -s ${ffmpeg}/bin/ffmpeg $out/ffmpeg-$FFMPEG_REVISION/ffmpeg-linux
-      '');
-
-  browsers-linux-arm64 = { withChromium ? true }:
+  browsers-linux = { withChromium ? true }:
     let
       fontconfig = makeFontsConf {
         fontDirectories = [ ];
@@ -186,7 +162,7 @@ EOF
 
         # See here for the Chrome options:
         # https://github.com/NixOS/nixpkgs/issues/136207#issuecomment-908637738
-        makeWrapper ${chromium}/bin/chromium $out/chromium_headless_shell-$CHROMIUM_REVISION/chrome-linux/chrome \
+        makeWrapper ${chromium}/bin/chromium $out/chromium_headless_shell-$CHROMIUM_REVISION/chrome-linux/headless_shell \
           --set SSL_CERT_FILE /etc/ssl/certs/ca-bundle.crt \
           --set FONTCONFIG_FILE ${fontconfig}
       '' + ''
